c6932e5183aee50ca50b45ad9bf602413cad43df
Hobbit-Klaus <hwchon1234@naver.com>
2016-03-28 01:52:56 +0900

    querystring을 이용해 writer 출력

--- Basic-Server/app.js
+++ Basic-Server/app.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 http.createServer(function(req, res) {
   res.writeHead(200);
-  res.write(req.url);
+  res.write(querystring.parse(url.parse(req.url).query)['writer'] + '');
   res.end();
 }).listen(3000);
 
