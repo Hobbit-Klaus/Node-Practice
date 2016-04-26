ee0ed7fda503b31413b1bfc8fb4f694285ec1500
Hobbit-Klaus <hwchon1234@naver.com>
2016-04-26 10:27:25 +0900

    Using plugins

--- GettingStarted-Hapi/server.js
+++ GettingStarted-Hapi/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
@@ -8,7 +9,7 @@ server.connection({ port: 3000 });
 server.route({
     method: 'GET',
     path: '/',
-    handler: function(req, reply) {
+    handler: function (req, reply) {
         reply('Hello, World!');
     }
 });
@@ -16,32 +17,35 @@ server.route({
 server.route({
     method: 'GET',
     path: '/{name}',
-    handler: function(req, reply) {
+    handler: function (req, reply) {
         reply('Hello, ' + encodeURIComponent(req.params.name) + '!');
     }
 });
 
-server.start((err) => {
-    
-    if (err) {
-        throw err;
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
     }
-    
-    console.log('Server running at:', server.info.uri);
-    
-});
-
-server.register(require('inert'), (err) => {
+}, (err) => {
 
     if (err) {
-        throw err;
-    }
+        throw err;  // 플러그인을 로딩하던 중 에러났을때???
+    } else {
+        server.start((err) => {
 
-    server.route({
-        method: 'GET',
-        path: '/hello',
-        handler: function (request, reply) {
-            reply.file('./public/hello.html');
-        }
-    });
+            if (err) {
+                throw err;
+            }
+
+            server.log('info', 'Server running at: ' + server.info.uri);
+
+        });
+    }
 });
